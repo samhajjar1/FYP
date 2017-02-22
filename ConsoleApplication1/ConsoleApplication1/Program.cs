5570004723946e5b87117a35e0ebb447aa123243
Marc <marcnohra@hotmail.com>
2017-02-22 17:46:47 +0200

    first commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Salut Marc");
+            Console.WriteLine("Salut Sam");
         }
     }
 }
