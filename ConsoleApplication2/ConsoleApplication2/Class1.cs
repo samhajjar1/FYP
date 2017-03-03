39942173d8704ef8c8904b2630af0615258e2863
sam hajjar <sam.hajjar@hotmail.com>
2017-03-03 17:03:58 +0200

    added getPageInfo and getAllPosts methodes

---
+++ ConsoleApplication2/ConsoleApplication2/Class1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Class1
+    {
+
+    }
+
+    public class FacebookPageInfo
+    {
+        public static string fields = "?fields=id,name,about,awards,category,checkins,description,cover,emails,fan_count,link,location,overall_star_rating,phone,rating_count,single_line_address,talking_about_count,website,were_here_count";
+        public long id { get; set; }
+        public string name { get; set; }
+        public string about { get; set; }
+        public string awards { get; set; }
+        public string category { get; set; }
+        public int checkins { get; set; }
+        public string description { get; set; }
+        public Cover cover { get; set; }
+        public List<string> emails { get; set; }
+        public int fan_count { get; set; }
+        public string link { get; set; }
+         public Location location { get; set; }
+        public int overall_star_rating { get; set; }
+        public string phone { set; get; }
+        public int rating_count { set; get; }
+        public string single_line_address { set; get; }
+        public int talking_about_count { get; set; }
+        public string website { get; set; }
+        public int were_here_count { get; set; }
+    }
+
+    public class Location
+    {
+        //todo
+    }
+
+    public class Cover
+    {
+        //todo
+    }
+
+    public class FacebookPost
+    {
+
+        public static string fields = "?fields=id,message,created_time,link";
+        public string message { get; set; }
+        public string created_Time { get; set; }
+        public string id { get; set; }
+        public string link { get; set; }
+
+
+
+
+    }
+
+    public class FacebookPagingInfo
+    {
+        public string Previous { get; set; }
+        public string Next { get; set; }
+    }
+
+    public class FacebookPostData
+    {
+        public List<FacebookPost> Data { get; set; }
+        public FacebookPagingInfo Paging { get; set; }
+    }
+
+
+
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook;
+using System.Net;
+using static System.Net.Mime.MediaTypeNames;
+using Newtonsoft.Json;
+
+namespace ConsoleApplication2
+{
+
+    class Program
+    {
+        private FacebookClient myfbclient;
+        private string JavistaPage = "/248586058548523";
+
+        public Program ()
+        {
+            string AppId = "389520154761799";
+            string AppSecret = "e4668b50fc17094e8b88f30774ad11e1";
+ 
+            var client = new WebClient();
+            string oauthUrl = string.Format("https://graph.facebook.com/oauth/access_token?type=client_cred&client_id={0}&client_secret={1}", AppId, AppSecret);
+
+            string accessToken = client.DownloadString(oauthUrl).Split('=')[1];
+            myfbclient = new FacebookClient(accessToken);
+            
+        }
+
+        public FacebookPageInfo getPageInfo(string pageId)
+        {
+            var pageData = myfbclient.Get(pageId + FacebookPageInfo.fields).ToString();
+            var info = JsonConvert.DeserializeObject<FacebookPageInfo>(pageData);
+            return info;
+        }
+
+        public List<FacebookPost> getAllFBPostsForPage(string pageId)
+        {
+           var pageData = myfbclient.Get(pageId+ "/posts"+FacebookPost.fields).ToString();
+            FacebookPostData posts = null ;
+            List<FacebookPost> postsLists = new List<FacebookPost>();
+            do
+            {
+                if (posts != null && posts.Data.Count != 0)
+                {
+                    pageData = myfbclient.Get(posts.Paging.Next).ToString();
+
+                }
+                posts = JsonConvert.DeserializeObject<FacebookPostData>(pageData);
+                postsLists.AddRange(posts.Data);
+            }
+            while (posts.Data.Count != 0);
+            return postsLists;
+        }
+
+        static void Main(string[] args)
+        {
+            Program myProgram = new Program();
+            FacebookPageInfo pageInfo = myProgram.getPageInfo(myProgram.JavistaPage);
+            List<FacebookPost> postsLists = myProgram.getAllFBPostsForPage(myProgram.JavistaPage);
+            FacebookClient
+        }
+    }
+}
